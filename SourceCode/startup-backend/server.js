5b2d6cfc4ff96cca1fbc27ebc6d74bfc99c3424c
Ch00mba <ellis.gove@students.plymouth.ac.uk>
2026-01-06 14:18:31 +0000

    fixed mongoose connection

--- SourceCode/startup-backend/server.js
+++ SourceCode/startup-backend/server.js
@@ -20,13 +20,21 @@ app.use('/api', discoveryRoutes);
 
 
 //Mongo Connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error', err));
 
+//test route
+
+app.get("/api/test-db", async (req, res) => {
+    try {
+        const result = await mongoose.connection.db.admin().ping();
+        res.json({connected:true, result});
+    } catch (err) {
+        res.status(500).json({connected: false, error: err.message});
+    }
+});
+
 //start
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
