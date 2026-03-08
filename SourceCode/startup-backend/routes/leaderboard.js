b26bd18e7e692fd825a20c229198d5b5ca0f512a
Ch00mba <ellis.gove@students.plymouth.ac.uk>
2026-03-08 23:13:46 +0000

    possible render fix

--- SourceCode/startup-backend/routes/leaderboard.js
+++ SourceCode/startup-backend/routes/leaderboard.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("..models/User");
+const User = require("..models/User.js");
 
 router.get("/leaderboard", async (req, res) => {
     try {
